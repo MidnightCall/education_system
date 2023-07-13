b49a10fefa0217535cad9d61f08a9bd8e827cc97
kojikoji <1310402980@qq.com>
2023-07-13 14:14:46 +0800

    Initial commit

---
+++ src/main/java/com/edu/EducationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/edu/commons/Result.java
@@ -0,0 +1,10 @@
+package com.edu.commons;/**
+ *@ClassName Result
+ *@Description  
+ *@Author kojikoji 1310402980@qq.com
+ *@Date 2023/7/13 13:48
+ *@Version 
+ */
+
+public class Result {
+}
