2b2e90499ab8257d73e1aa4aa9695bdcb17937b2
kojikoji <1310402980@qq.com>
2023-07-14 16:24:07 +0800

    Student类，鉴权

--- doc/sql/education.sql
+++ doc/sql/education.sql
@@ -1,29 +1,176 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : wch
+ Source Server Type    : MySQL
+ Source Server Version : 50718
+ Source Host           : sh-cynosdbmysql-grp-r4gyhpjo.sql.tencentcdb.com:27871
+ Source Schema         : education
+
+ Target Server Type    : MySQL
+ Target Server Version : 50718
+ File Encoding         : 65001
+
+ Date: 14/07/2023 16:19:52
+*/
+drop database if exists education;
+create database education;
 use education;
 
-drop table if exists equipment;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-create table equipment (
-   equipment_id char(10) CHARACTER SET utf8mb4 comment '设备id',
-   equipment_name varchar(50) CHARACTER SET utf8mb4 default null comment '设备名称',
-   equipment_type varchar(20) CHARACTER SET utf8mb4 default null comment '设备类型',
-   PRIMARY KEY (`equipment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='设备管理配置';
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-BEGIN;
-INSERT INTO `equipment` VALUES ('1000000001', '丁真的小马', '纯真');
-INSERT INTO `equipment` VALUES ('1000000002', '华为13 pro max 冷锋蓝', '爱国');
-COMMIT;
+-- ----------------------------
+-- Records of department
+-- ----------------------------
+INSERT INTO `department` VALUES (101, '教务处', '行政楼');
+INSERT INTO `department` VALUES (102, '学工部', '行政楼');
 
-drop table if exists laboratory;
+-- ----------------------------
+-- Table structure for equipment
+-- ----------------------------
+DROP TABLE IF EXISTS `equipment`;
+CREATE TABLE `equipment`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `price` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `purchase_time` date NOT NULL,
+  `lab_id` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 315286489 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-create table laboratory (
-    laboratory_id char(8) CHARACTER SET utf8mb4 comment '实验室',
-    laboratory_name varchar(50) CHARACTER SET utf8mb4 default null comment '实验室名称',
-    laboratory_type varchar(20) CHARACTER SET utf8mb4 default null comment '实验室地址',
-   PRIMARY KEY (`laboratory_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='实验室管理配置';
+-- ----------------------------
+-- Records of equipment
+-- ----------------------------
+INSERT INTO `equipment` VALUES (315286452, '瑞克5', '呜呼呜呼真的好满足', '996', '电子烟', '2023-07-14', '78128505437');
+INSERT INTO `equipment` VALUES (315286488, '芙蓉王', '从理塘到了上海，哥们收获好多money', '996', '传统香烟', '2023-07-14', '20785587425');
 
-BEGIN;
-INSERT INTO `laboratory` VALUES ('10000001', '理塘', '世界最高城');
-INSERT INTO `laboratory` VALUES ('10000002', '四川大学男厕所', 'null');
-COMMIT;
+-- ----------------------------
+-- Table structure for laboratory
+-- ----------------------------
+DROP TABLE IF EXISTS `laboratory`;
+CREATE TABLE `laboratory`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `department_id` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher_id` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1679755765482680321 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of laboratory
+-- ----------------------------
+INSERT INTO `laboratory` VALUES (1679755761070272512, '理塘', '世界最高城', '101', '100001');
+INSERT INTO `laboratory` VALUES (1679755765482680320, '四川大学男厕所', 'null', '102', '100002');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `gender` int(11) NOT NULL DEFAULT 0,
+  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `department_id` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `grade` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `class` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `dormitory` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1679764891343933442 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1679764887720054786, '丁真的小马', 0, '114514', '101', '2020', '2020104', '201A');
+INSERT INTO `student` VALUES (1679764891343933441, '陈睿的马', 0, '114514', '102', '2020', '2020104', '201B');
+
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `gender` int(11) NOT NULL DEFAULT 0,
+  `department_id` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `job` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `salary` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 315286967 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES (315286315, '芙蓉王源', 0, '101', 'negro', '114514', '114514');
+INSERT INTO `teacher` VALUES (315286966, '尼古丁真', 0, '101', 'rapper', '114514', '114514');
+
+-- ----------------------------
+-- Table structure for teacher_performance
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_performance`;
+CREATE TABLE `teacher_performance`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `teacher_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `year` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `teacher_grade` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `reacher_grade` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher_performance
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for teacher_salary
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_salary`;
+CREATE TABLE `teacher_salary`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `teacher_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `salary` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `benefits` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacher_salary
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `username` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键，用户名及登录账号',
+  `password` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  PRIMARY KEY (`username`) USING BTREE,
+  UNIQUE INDEX `user_username_uindex`(`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('admin', '123456');
+INSERT INTO `user` VALUES ('admin1', '123456');
+INSERT INTO `user` VALUES ('admin2', '123456');
+INSERT INTO `user` VALUES ('admin3', '123456');
+INSERT INTO `user` VALUES ('admin5', '123456');
+
+SET FOREIGN_KEY_CHECKS = 1;
