3bba8f03636b3282ed561998639e195b0f6b4a00
Jiacheng Zhu <2127075405@qq.com>
2023-07-15 21:26:14 +0800

    Growth

---
+++ src/main/java/com/edu/mapper/GrowthMapper.java
@@ -0,0 +1,14 @@
+package com.edu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.edu.entity.Growth;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Auther: 朱嘉诚
+ * @Date: 2023/7/15 18:49
+ * @Description:
+ */
+@Mapper
+public interface GrowthMapper extends BaseMapper<Growth> {
+}
